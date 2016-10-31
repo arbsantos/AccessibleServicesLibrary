2cacdbf7a23ea99e24266ab97fd7ed5d1826cf2f
André Santos <andre13521@gmail.com>
2016-10-31 11:05:02 +0000

    Inicial Specification

---
+++ app/src/main/java/pt/ul/fc/di/lasige/accessibleserviceslibrary/Library.java
@@ -0,0 +1,70 @@
+package pt.ul.fc.di.lasige.accessibleserviceslibrary;
+
+import android.view.accessibility.AccessibilityEvent;
+import android.view.accessibility.AccessibilityNodeInfo;
+
+
+public interface Library {
+
+    /**
+     * Given a AccessibilityNodeInfo returns its description
+     * if any
+     * @param node
+     * @return String with the descr
+     */
+    public String getDescription(AccessibilityNodeInfo node);
+
+    /**
+     * Given a AccessibilityNodeInfo returns its  parent description
+     * if any
+     * @param node
+     * @return String with the descr
+     */
+    public String getParentDescription(AccessibilityNodeInfo node);
+
+    /**
+     * Given a AccessibilityNodeInfo returns its child description
+     * if any
+     * @param node
+     * @return String with the descr
+     */
+    public String getChildDescription(AccessibilityNodeInfo node);
+
+    /**
+     * Given a AccessibilityNodeInfo returns its package
+     * if any
+     * @param node
+     * @return String with the package
+     */
+    public String getPackageName(AccessibilityNodeInfo node);
+
+    /**
+     * Given a AccessibilityNodeInfo returns its class
+     * if any
+     * @param node
+     * @return String with the package
+     */
+    public String getClassName(AccessibilityNodeInfo node);
+
+    /**
+     *
+     * @param event
+     * @return
+     */
+    public String getEventType(AccessibilityEvent event);
+
+    /**
+     * Search for a node on screen
+     * @param description
+     * @return
+     */
+    public AccessibilityNodeInfo searchNode(String description);
+
+    /**
+     * Log on loogcat all nodes on the screen
+     * @param node
+     */
+    public void logNodeTree(AccessibilityNodeInfo node);
+
+
+}
---
+++ app/src/main/java/pt/ul/fc/di/lasige/accessibleserviceslibrary/MainActivity.java
@@ -0,0 +1,13 @@
+package pt.ul.fc.di.lasige.accessibleserviceslibrary;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
